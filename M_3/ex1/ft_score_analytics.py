f57686063d676fa3f2b508190b94b0027683ae00
denys <denysbonfim1@gmail.com>
2026-05-21 11:18:44 +0100

    all

--- M_3/ex1/ft_score_analytics.py
+++ M_3/ex1/ft_score_analytics.py
@@ -5,12 +5,23 @@ def main() -> None:
     print("\033[42;31;40m=== Player Score Analytics ===\033[m")
     lista = list()
     c: int = 1
-    try:
-        if len(sys.argv) <= 1:
-            raise IndexError("<score1> <score2> ...")
-        while c < len(sys.argv):
+    if len(sys.argv) <= 1:
+        print("<score1> <score2> ...")
+        return
+    while c < len(sys.argv):
+        try:
             lista.append(int(sys.argv[c]))
             c += 1
+        except IndexError as e:
+            print(f"No scores provided: try {e}")
+            c += 1
+            continue
+        except ValueError as e:
+            print(f"Not number provides: {e}")
+            c += 1
+            continue
+    if len(lista) >= 1:
+        print(f"Score range: {max(lista) - min(lista)}")
         print(f"Scores processed: {lista}")
         print(f"Total players: {len(lista)}")
         print(f"Total score {sum(lista)}")
@@ -18,11 +29,6 @@ def main() -> None:
         print(f"Average score: {score}")
         print(f"Max score: {max(lista)}")
         print(f"Min score: {min(lista)}")
-        print(f"Score range: {max(lista) - min(lista)}")
-    except IndexError as e:
-        print(f"No scores provided: try {e}")
-    except ValueError as e:
-        print(f"Not number provides: {e}")
 
 
 if __name__ == '__main__':
